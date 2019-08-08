24227b9c52dd8bcdaaab217e36615d41cbc98b8d
Lucas Sproat <lucassproat@gmail.com>
2019-08-07 23:37:53 -0500

    Camera switch prototype functionality
    - Added simple camera switch on pickup collision
    - Added 3D platform for FP movement
    - Altered 2D/FP Player organization in hiearchy

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] GameObject fpPlayer;
+    [SerializeField] GameObject player2D;
+
+    private void Awake()
+    {
+        fpPlayer.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CamSwitch")
+        {
+            fpPlayer.SetActive(true);
+            player2D.SetActive(false);
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement2D : MonoBehaviour
 {
 
      Rigidbody body;
