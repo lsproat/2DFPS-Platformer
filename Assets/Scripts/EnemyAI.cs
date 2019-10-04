d67001c79de554fe3b31e074b58a485aebbe6875
Lucas <lucassproat@gmail.com>
2019-10-04 14:16:18 -0500

    Progress on AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,58 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
 
-    [SerializeField] private float timeBetweenPoints = 1f;
-    [SerializeField] List<Vector3> walkPoints;
+    [SerializeField] Transform playerTarget;
+    [SerializeField] float turnSpeed = 5f;
+    [SerializeField] float waitBeforeMove = 3f;
+    public Vector3[] points;
+    public bool playerDetected = false;
 
-    float currentLerpTime;
-    int count = 0;
-    int count2 = 0;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+    private int destPoint = 0;
 
-    bool dectectedPlayer = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.autoBraking = false; //continuous motion between points (no slowdown)
+        navMeshAgent.updateRotation = false;
 
-    // Update is called once per frame
+        GotoNextPoint();
+    }
+
     void Update()
     {
-        
+        // distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (!playerDetected && !navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+            //GotoNextPoint();
+            WaitAndRotate();
+       //else if (playerDetected) EngageTarget();
     }
 
-    private void Start()
+    void GotoNextPoint()
     {
-        IdleMovement();
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        navMeshAgent.destination = points[destPoint];
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 
-    void IdleMovement()
+    private void EngageTarget()
     {
-        while (!dectectedPlayer)
+        FaceTarget(playerTarget.position);
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
         {
-            if (count == walkPoints.Count - 1) count2 = 0;
-            else count2 = count++;
-            //MoveFromAToB(walkPoints[count], walkPoints[count2]);
-            Invoke("MoveFromAToB", timeBetweenPoints);
+            ChaseTarget();
+        }
 
-            count++;
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            //capture/attack
         }
     }
 
-    void MoveFromAToB()
+    private void ChaseTarget()
     {
-        Vector3 startPos = walkPoints[count];
-        Vector3 endPos = walkPoints[count2];
+        navMeshAgent.SetDestination(playerTarget.position);
+    }
 
-        currentLerpTime += Time.deltaTime;
-        if (currentLerpTime > timeBetweenPoints)
-        {
-            currentLerpTime = timeBetweenPoints;
-        }
+    IEnumerator WaitAndRotate()
+    {
+        //int tmpDestPoint = (destPoint + 1) % points.Length;
+        Vector3 tmpDestination = points[destPoint];
+        FaceTarget(tmpDestination);
+        yield return new WaitForSeconds(waitBeforeMove);
+        GotoNextPoint();
 
-        //lerp!
-        float perc = currentLerpTime / timeBetweenPoints;
-        transform.position = Vector3.Lerp(startPos, endPos, perc);
-        // TODO: Lerp to look at next target
-        //transform.rotation = Quaternion.Lerp(startRot, endRot, perc);
+    }
+
+    private void FaceTarget(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // shows detected radius (not being used)
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
